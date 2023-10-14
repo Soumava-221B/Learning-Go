c5d47b7058c765100a8681eba1c953c9f16bd5e8
soumava <soumavadas06@gmail.com>
2023-10-14 20:39:32 +0530

    range

---
+++ range.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	nums := []int{4, 3, 9}
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+	fmt.Println("sum:", sum)
+
+	for i, num := range nums {
+		if num == 3 {
+			fmt.Println("index:", i)
+		}
+	}
+
+	kvs := map[string]string{"c": "carrot", "b": "brinjal"}
+	for k, v := range kvs {
+		fmt.Printf("%s -> %s\n", k, v)
+	}
+
+	for k := range kvs {
+		fmt.Println("key:", k)
+	}
+
+	for i, c := range "go" {
+		fmt.Println(i, c)
+	}
+}
